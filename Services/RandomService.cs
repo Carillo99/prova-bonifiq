306845b7b0842c8277982b0e48d88b13aff4a73f
Gleisson Carillo de Souza <mailto:devecommerce3@belenus.com.br>
2025-03-12 22:51:09 -0300

    fix: Ajuste para retornar em cada execução um número diferente

--- Services/RandomService.cs
+++ Services/RandomService.cs
@@ -6,24 +6,30 @@ namespace ProvaPub.Services
 {
 	public class RandomService
 	{
-		int seed;
         TestDbContext _ctx;
 		public RandomService()
         {
             var contextOptions = new DbContextOptionsBuilder<TestDbContext>()
-    .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Teste;Trusted_Connection=True;")
-    .Options;
-            seed = Guid.NewGuid().GetHashCode();
+                .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Teste;Trusted_Connection=True;")
+                .Options;
 
             _ctx = new TestDbContext(contextOptions);
         }
         public async Task<int> GetRandom()
 		{
-            var number =  new Random(seed).Next(100);
-            _ctx.Numbers.Add(new RandomNumber() { Number = number });
-            _ctx.SaveChanges();
-			return number;
-		}
+			try
+			{
+                var seed = Guid.NewGuid().GetHashCode();
+                var number = new Random(seed).Next(100);
+                _ctx.Numbers.Add(new RandomNumber() { Number = number });
+                _ctx.SaveChanges();
 
+                return number;
+            }
+			catch (Exception)
+			{
+                return -1;
+			}
+		}
 	}
 }
